a4567141552d1459e53f53cd487c72f29f165107
Rossen Georgiev <zoorty@gmail.com>
2015-07-07 00:03:16 +0100

    remove py2.6 from test; no longer supported

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         'Topic :: Text Processing ',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.0',
