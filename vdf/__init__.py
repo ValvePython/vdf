29989b734de76d77802ee82d4fba5e9752489799
SleepProgger <SleepProgger@users.noreply.github.com>
2016-03-12 04:48:33 +0100

    Initial binary parse/dump

--- vdf/__init__.py
+++ vdf/__init__.py
@@ -2,6 +2,7 @@
 Module for deserializing/serializing to and from VDF
 """
 __version__ = "1.10"
+import struct
 __author__ = "Rossen Georgiev"
 
 import re
@@ -110,6 +111,85 @@ def parse(fp, mapper=dict):
     return stack.pop()
 
 
+def _read_till_seperator(fp, seperator="\x00", buffersize=2048):
+    tmp = ""
+    start_offset = fp.tell() 
+    _sep_length = len(seperator)
+    while True:
+        data = fp.read(buffersize)
+        if not data:
+            return tmp
+        tmp += data
+        index = tmp.find(seperator)
+        if index != -1:
+            fp.seek(start_offset + index + _sep_length)
+            return tmp[:index]
+
+
+BIN_NONE = '\x00'
+BIN_STRING = '\x01'
+BIN_INT32 = '\x02'
+BIN_FLOAT32 = '\x03'
+BIN_POINTER = '\x04'
+BIN_WIDESTRING = '\x05'
+BIN_COLOR = '\x06'
+BIN_UINT64 = '\x07'
+BIN_END = '\x08'
+def parse_binary(source, mapper=dict):
+    """
+    Deserialize ``source`` (a ``str`` or file like object containing a VDF in "binary form")
+    to a Python object.
+
+    ``mapper`` specifies the Python object used after deserializetion. ``dict` is
+    used by default. Alternatively, ``collections.OrderedDict`` can be used if you
+    wish to preserve key order. Or any object that acts like a ``dict``.
+    """
+    if not issubclass(mapper, dict):
+        raise TypeError("Expected mapper to be subclass of dict, got %s", type(mapper))
+    if hasattr(source, 'read'):
+        fp = source
+    elif isinstance(source, string_type):
+        fp = strIO(source)
+    else:
+        raise TypeError("Expected source to be str or file-like object")
+
+    # init
+    stack = [mapper()]    
+    _read_int32 = struct.Struct('<i').unpack
+    _read_uint64 = struct.Struct('<Q').unpack
+    _read_float32 = struct.Struct('<f').unpack
+
+    while True:
+        obj_type = fp.read(1)
+        if obj_type == BIN_END:
+            if len(stack) > 1:
+                stack.pop()
+            else:
+                return stack[0]
+            continue
+        
+        obj_name = _read_till_seperator(fp, seperator="\x00")
+        if obj_type == BIN_NONE:
+            stack[-1][obj_name] = mapper()
+            stack.append(stack[-1][obj_name])
+        elif obj_type == BIN_STRING:
+            value = _read_till_seperator(fp, seperator="\x00")
+            stack[-1][obj_name] = value
+        elif obj_type == BIN_INT32:
+            stack[-1][obj_name] = _read_int32(fp.read(4))[0]
+        elif obj_type == BIN_UINT64:
+            stack[-1][obj_name] = _read_uint64(fp.read(8))[0]
+        elif obj_type == BIN_FLOAT32:
+            stack[-1][obj_name] = _read_float32(fp.read(4))[0]
+        elif obj_type in (BIN_POINTER, BIN_WIDESTRING, BIN_COLOR):
+            # TODO: Check what they are and implement
+            raise SyntaxError('vdf.parse_binary: type not supported #%i' % ord(obj_type))
+        else:
+            raise SyntaxError('vdf.parse_binary: invalid type code #%i' % ord(obj_type))
+       
+    return stack.pop()
+
+
 def loads(s, **kwargs):
     """
     Deserialize ``s`` (a ``str`` or ``unicode`` instance containing a JSON
@@ -175,3 +255,32 @@ def _dump_gen(data, pretty=False, level=0):
             yield "%s}\n" % line_indent
         else:
             yield '%s"%s" "%s"\n' % (line_indent, key, value)
+            
+            
+def _dump_gen_binary(data, level=0):
+    """
+    Serializes an dict (or an extension thereof) as binary vdf.
+    Every scalar need to be a tuple or list with the length 2 in the form:
+        (``data_type``, ``value``)
+        where ``data_type`` is one of (BIN_INT32, BIN_UINT64, BIN_FLOAT32, BIN_STRING)
+    """
+    type_mapper = {
+                   BIN_INT32: struct.Struct('<i').pack,
+                   BIN_UINT64: struct.Struct('<Q').pack,
+                   BIN_FLOAT32: struct.Struct('<f').pack,
+                   BIN_STRING: lambda x: x + "\x00",
+                   }
+    for key, value in data.items():
+        if isinstance(value, dict):
+            yield "".join((BIN_NONE, key, "\x00"))
+            for chunk in _dump_gen_binary(value, level+1):
+                yield chunk
+            yield BIN_END
+        else:
+            if not isinstance(value, (list, tuple)) or len(value) != 2:
+                raise TypeError("Values need to be a list or tuple with the length 2.")
+            type_code, type_data = value
+            if type_code in type_mapper:
+                yield "".join((type_code, key, "\x00", type_mapper[type_code](type_data)))
+            else:
+                raise TypeError('Unsupported type')
