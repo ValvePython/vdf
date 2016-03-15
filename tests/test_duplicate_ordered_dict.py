ebb1805a549d57beb8ce474fd5506068386dce94
SleepProgger <SleepProgger@users.noreply.github.com>
2016-03-15 22:44:17 +0100

    Refactored VDFDict

--- tests/test_duplicate_ordered_dict.py
+++ tests/test_duplicate_ordered_dict.py
@@ -1,58 +1,90 @@
 import unittest
-from vdf import DuplicateOrderedDict
+from vdf import VDFDict
 
 
 class DuplicateOrderedDict_test(unittest.TestCase):
     map_test = (
-            (1, 2),
-            (4, 3),(4, 3),(4, 2),
-            (7, 2),
-            (1, 2),
+            ("1", 2),
+            ("4", 3),("4", 3),("4", 2),
+            ("7", 2),
+            ("1", 2),
         )
 
     def test_keys(self):
-        _dict = DuplicateOrderedDict(self.map_test)
+        _dict = VDFDict(self.map_test)
         self.assertSequenceEqual(
             tuple(_dict.keys()),
             tuple(x[0] for x in self.map_test))
         
     def test_values(self):
-        _dict = DuplicateOrderedDict(self.map_test)
+        _dict = VDFDict(self.map_test)
         self.assertSequenceEqual(
             tuple(_dict.values()),
             tuple(x[1] for x in self.map_test))
         
     def test_items(self):
-        _dict = DuplicateOrderedDict(self.map_test)
+        _dict = VDFDict(self.map_test)
         self.assertSequenceEqual(
             tuple(_dict.items()),
             self.map_test)
         
     def test_direct_access_set(self):
-        a = {1:2, 3:4, 5:6}
-        b = DuplicateOrderedDict()
+        a = {"1":2, "3":4, "5":6}
+        b = VDFDict()
         for k,v in a.items():
             b[k] = v
         self.assertDictEqual(a, b)
         
     def test_direct_access_get(self):
         b = dict()
-        a = DuplicateOrderedDict({1:2, 3:4, 5:6})
+        a = VDFDict({"1":2, "3":4, "5":6})
         for k,v in a.items():
             b[k] = v
         self.assertDictEqual(a, b)
         
     def test_update(self):
-        a = DuplicateOrderedDict(((1,2),(1,2),(5,3),(1,2)))
-        b = DuplicateOrderedDict()
-        b.update(((1,2),(1,2)))
-        b.update(((5,3),(1,2)))
+        a = VDFDict((("1",2),("1",2),("5",3),("1",2)))
+        b = VDFDict()
+        b.update((("1",2),("1",2)))
+        b.update((("5",3),("1",2)))
         self.assertSequenceEqual(tuple(a.items()), tuple(b.items()))
         
     def test_update_2(self):
         self.assertSequenceEqual(
-            tuple(DuplicateOrderedDict(self.map_test).items()),
-            tuple(DuplicateOrderedDict(DuplicateOrderedDict(self.map_test)).items()))
+            tuple(VDFDict(self.map_test).items()),
+            tuple(VDFDict(VDFDict(self.map_test)).items()))
+        
+    def test_del(self):
+        """ Tests del """
+        a = VDFDict((("1",2),("1",2),("5",3),("1",2)))
+        b = VDFDict((("1",2),("1",2),("1",2)))
+        del a["5"]
+        self.assertSequenceEqual(a.items(), b.items())
+        
+    def test_remove_all(self):
+        a = VDFDict((("1",2),("1",2),("5",3),("1",2)))
+        b = VDFDict((("5",3),))
+        a.remove_all_by_key("1")
+        self.assertSequenceEqual(a.items(), b.items())
+        
+    def test_clear(self):
+        a = VDFDict((("1",2),("1",2),("5",3),("1",2)))
+        a.clear()
+        self.assertEqual(len(a), 0)
+        
+        
+    def test_exception_insert(self):
+        """ Only strings (and tuples) are supported as keys """
+        a = VDFDict()
+        self.assertRaises(TypeError, a.__setitem__, 5, "foo")
+        
+    def test_exception_remove_all(self):
+        """ Only strings are supported as keys """
+        a = VDFDict()
+        self.assertRaises(TypeError, a.remove_all_by_key, 5)
+        
+        
+        
         
         
         
--- vdf/__init__.py
+++ vdf/__init__.py
@@ -5,6 +5,8 @@ __version__ = "1.10"
 import collections
 __author__ = "Rossen Georgiev"
 
+from vdfDict import VDFDict
+
 import re
 import sys
 import struct
@@ -27,82 +29,6 @@ else:
 
     def strip_bom(line):
         return line.lstrip(BOMS if isinstance(line, str) else BOMS_UNICODE)
-
-
-
-class DuplicateOrderedDict(dict):
-    def __init__(self, values=None):
-        """
-        A dictionary implmentation which allows duplicate keys and contains the insert order.
-        
-    
-        ``values`` can be used to initialize this `DuplicateOrderedDict` instance..
-          Dict like objects and iterables containing iterables of the length 2 are supported. 
-        """
-        self.__omap = []
-        if not values is None:
-            self.update(values)
-                
-    def __repr__(self):
-        out = "%s(" % self.__class__.__name__
-        out += "%s)" % repr(self.items())
-        return out
-    
-    def __len__(self):
-        return len(self.__omap)
-    
-    def __setitem__(self, key, value):
-        if not isinstance(key, tuple):
-            idx = 0
-            while True:
-                if (idx, key) not in self:
-                    self.__omap.append((idx, key))
-                    break
-                else:
-                    idx += 1
-            key = (idx, key)
-        else:
-            if key not in self:
-                raise KeyError("%s doesn\'t exist" % repr(key))      
-        super(DuplicateOrderedDict, self).__setitem__(key, value)
-        
-    def __getitem__(self, key):
-        if not isinstance(key, tuple):
-            key = (0, key)
-        return super(DuplicateOrderedDict, self).__getitem__(key)
-    
-    def __iter__(self):
-        return iter(self.keys())
-    
-    def __eq__(self, other):
-        if isinstance(other, dict):
-            other = tuple(other.items())
-        return other == tuple(self.items())
-    
-    def __ne__(self, other):
-        return not self.__eq__(other) 
-    
-    def items(self):
-        return tuple((key[1], self[key]) for key in self.__omap)
-    
-    def keys(self):
-        return tuple(key[1] for key in self.__omap)
-    
-    def values(self): 
-        return tuple(self[key] for key in self.__omap)
-
-    def update(self, data=None, **kwargs):
-        if not data is None:
-            if hasattr(data, 'items'):
-                data = data.items()
-            if not isinstance(data, collections.Iterable):
-                raise TypeError('Argument need to provide a items method or be iterable.')
-            for kv in data:
-                if len(kv) != 2:
-                    raise TypeError('Argument, or its keys method need to provide tuples of the length 2.')
-                self[kv[0]] = kv[1]
-        if len(kwargs) > 0:
-            self.update(kwargs)
             
 
 
