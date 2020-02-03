6da1af90467aadc1d89c7a8c1d36a4a811df85fe
Lassi Säike <lassisaike11@gmail.com>
2020-02-03 22:54:48 +0200

    Added support for single / inside unquoted values

--- vdf/__init__.py
+++ vdf/__init__.py
@@ -89,7 +89,7 @@ def parse(fp, mapper=dict, merge_duplicate_keys=True, escaped=True):
     re_keyvalue = re.compile(r'^("(?P<qkey>(?:\\.|[^\\"])+)"|(?P<key>#?[a-z0-9\-\_\\\?$%]+))'
                              r'([ \t]*('
                              r'"(?P<qval>(?:\\.|[^\\"])*)(?P<vq_end>")?'
-                             r'|(?P<val>[a-z0-9\-\_\\\?\*\.]+)'
+                             r'|(?P<val>(?:(?<!/)/(?!/)|[a-z0-9\-\_\\\?\*\.])+)'
                              r'))?',
                              flags=re.I)
 
