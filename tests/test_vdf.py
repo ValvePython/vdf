74fb682f1a250607ce0969ee6a85e8afb770e180
Lassi Säike <lassisaike11@gmail.com>
2020-02-03 23:41:53 +0200

    Updated tests for new symbols & empty blocks

--- tests/test_vdf.py
+++ tests/test_vdf.py
@@ -319,11 +319,12 @@ class testcase_VDF(unittest.TestCase):
         self.assertEqual(vdf.loads(INPUT, escaped=False), EXPECTED)
 
     def test_wierd_symbols_for_unquoted(self):
-        INPUT = 'a asd.vdf\nb language_*lol*\nc zxc_-*.sss//'
+        INPUT = 'a asd.vdf\nb language_*lol*\nc zxc_-*.sss//\nd<2?$% /cde/$fgh/<i>'
         EXPECTED = {
             'a': 'asd.vdf',
             'b': 'language_*lol*',
             'c': 'zxc_-*.sss',
+            'd<2?$%': '/cde/$fgh/<i>',
             }
 
         self.assertEqual(vdf.loads(INPUT), EXPECTED)
@@ -393,6 +394,39 @@ class testcase_VDF(unittest.TestCase):
 
         self.assertEqual(vdf.loads(INPUT, escaped=False), EXPECTED)
 
+    def test_single_line_empty_block(self):
+        INPUT = '''
+        "root1"
+        {
+            "key1" {}
+            key2 "value2"
+            "key3" value3
+        }
+        root2 { }
+        root3
+        {
+            "key1" "value1"
+            key2 {   }
+            "key3" value3
+        }
+        '''
+
+        EXPECTED = {
+            'root1': {
+                'key1': {},
+                'key2': 'value2',
+                'key3': 'value3',
+            },
+            'root2': {},
+            'root3': {
+                'key1': 'value1',
+                'key2': {},
+                'key3': 'value3',
+            }
+        }
+
+        self.assertEqual(vdf.loads(INPUT), EXPECTED)
+        self.assertEqual(vdf.loads(INPUT, escaped=False), EXPECTED)
 
 class testcase_VDF_other(unittest.TestCase):
     def test_dumps_pretty_output(self):
